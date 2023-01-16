3a47c32975dcd2ad98fb3eff51353a8a3977b900
asumpta <sumgathoni@gmail.com>
2023-01-16 10:04:55 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+// findMatching
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase().includes(name.toLowerCase()));
+}
+
+// fuzzyMatch
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+// matchName
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
+
+
+
+
+
+
